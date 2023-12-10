a3e15e719b4550d821f9801267006599e89f992f
Shivam Mishra <shivaymishraeee@gmail.com>
2023-12-10 16:51:27 +0530

    Final Commit from the frontend side

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,31 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import { FaRegFileAlt } from "react-icons/fa";
 import { MdOutlineFileDownload } from "react-icons/md";
+import { IoClose } from "react-icons/io5";
+import { motion } from "framer-motion"
 
-function Card() {
+function Card({ data,reference }) {
     return (
-        <div className='relative w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden'>
+        <motion.div drag dragConstraints={reference} whileDrag={{scale:1.1}} dragElastic={0.2} dragTransition={{ bounceStiffness: 200, bounceDamping: 8 }} className='relative flex-shrink-0 w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden'>
             <FaRegFileAlt />
-            <p className='text-sm mt-5 leading-tight font-semibold'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio incidunt dolor quos sunt sapiente rem.</p>
+            <p className='text-sm mt-5 leading-tight font-semibold'>{data.desc}</p>
             <div className='footer absolute w-full bottom-0 left-0'>
                 <div className='flex items-center justify-between py-3 px-8 mb-3'>
-                    <h5>.4mb</h5>
+                    <h5>{data.filesize}</h5>
                     <span className='w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center'>
-                    <MdOutlineFileDownload size=".9em" color="#fff"/>
+                        {data.close ? <IoClose /> : <MdOutlineFileDownload size=".9em" color="#fff" />}
                     </span>
                 </div>
-                <div className='tag w-full py-4 bg-green-500 flex items-center justify-center'>
-                    <h3 className='text-sm font-semibold'>Download Now</h3>
-                </div>
+                {
+                    data.tag.isOpen ? (<div className={`tag w-full py-4 ${data.tag.tagColor==="blue"?"bg-blue-600":"bg-green-600"} flex items-center justify-center`}>
+                        <h3 className='text-sm font-semibold'>{data.tag.tagTitle}</h3>
+                    </div>) : null
+                }
+
             </div>
-        </div>
+        </motion.div>
     )
 }
 
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,14 +1,47 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import Card from './Card'
 
 function Foreground() {
+  const ref=useRef(null)
+  const data = [
+    {
+      desc: "This is my 1st description of my card.",
+      filesize: ".9mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" }
+    },
+    {
+      desc: "This is my 2nd description of my card.",
+      filesize: ".4mb",
+      close: false,
+      tag: { isOpen: true, tagTitle: "Upload Now", tagColor: "blue" }
+    },
+    {
+      desc: "This is my 3rd description of my card.",
+      filesize: ".1mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" }
+    },
+    {
+      desc: "This is my 4th description of my card.",
+      filesize: ".3mb",
+      close: false,
+      tag: { isOpen: false, tagTitle: "Download Now", tagColor: "green" }
+    },
+  ];
   return (
-      <div className=' fixed top-0 left-0 z-[3] w-full h-full'>
-        <Card/>
-      </div>
-    
+    <div ref={ref} className=' fixed top-0 left-0 z-[3] w-full h-full flex gap-5 flex-wrap'>
+      {
+        data.map((item,index) => 
+          (
+            <Card data={item} key={index} reference={ref} />
+          )
+        )
+      }
+    </div>
+
   )
-} 
+}
 
 export default Foreground
